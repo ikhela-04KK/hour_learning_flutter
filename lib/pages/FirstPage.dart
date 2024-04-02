f6ed490f5fe75e23a4722ba671a59a1111e5a881
ikhela-04KK <kankoffi36@gmail.com>
2024-04-02 15:56:55 +0200

    use drawer() + navigator

--- lib/pages/FirstPage.dart
+++ lib/pages/FirstPage.dart
@@ -1,5 +1,5 @@
 
-// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -11,18 +11,61 @@ class FirstPage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text("first page"),
-      ), 
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Go to second page"),
-          onPressed: () => {
-            Navigator.pushNamed(
-              context,
-              '/secondpage'
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple[500],
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              child: Icon(
+                Icons.favorite, 
+                size: 48,
+              )
+            ), 
+
+            ListTile(
+              leading: Icon(
+                Icons.home, 
+                color: Colors.black,
+              ),
+              title: Text(
+                "Home", 
+                style: TextStyle(
+                  fontFamily: "JetBrainMono", 
+                  fontSize: 25
+                ),
+                ),
+            ), 
+            ListTile(
+              leading: Icon(
+                Icons.settings, 
+                color: Colors.black,
+              ),
+              title: Text(
+                "Seetings", 
+                style: TextStyle(
+                  fontFamily: "JetBrainMono", 
+                  fontSize: 25
+                ),
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, "/secondpage");
+                },
             )
-          },
+        ],
         ),
       ),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: Text("Go to second page"),
+      //     onPressed: () => {
+      //       Navigator.pushNamed(
+      //         context,
+      //         '/secondpage'
+      //       )
+      //     },
+      //   ),
+      // ),
     );
   }
 }
