f43ba922263b8ceca8d4c861c32fed67923caf26
ikhela-04KK <kankoffi36@gmail.com>
2024-04-02 03:51:28 +0200

    list-view and container

---
+++ android/app/src/main/kotlin/com/example/ikpay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ikpay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        // backgroundColor: Colors.deepPurple[200],
+        // appBar: AppBar(
+        //     title: Text(
+        //       'First Application',
+        //       style: TextStyle(color: Colors.white),
+        //     ),
+        //     backgroundColor: Colors.deepPurple[900],
+        //     centerTitle: true, // centrer le texte qui dans le AppBar
+        //     elevation: 0,
+        //     leading: IconButton(
+        //       icon: Icon(Icons.menu),
+        //       onPressed: () => {print("button is pressed")},
+        //       color: Colors.white,
+        //     ),
+        //     actions: <Widget>[
+        //       IconButton(
+        //         icon: const Icon(Icons.logout),
+        //         tooltip: 'Open shopping cart',
+        //         onPressed: () {
+        //           // handle the press
+        //           print("button is pressed");
+        //         },
+        //         color: Colors.white,
+        //       ),
+        //     ]),
+        // body: Center(
+        //   child: Container(
+        //     height: 300,
+        //     width: 300,
+        //     padding: EdgeInsets.all(25),
+        //     decoration: BoxDecoration(
+        //         color: Colors.deepPurple[900],
+        //         borderRadius:
+        //             BorderRadius.circular(20) //  sert à décorer la boxe
+        //         ),
+        //     // child: Text(
+        //     //   "Koffi Kan Marc",
+        //     //   style: TextStyle(
+        //     //     color: Colors.white,
+        //     //     fontSize: 28,
+        //     //     fontFamily: "JetBrainsMono",
+        //     //     fontWeight: FontWeight.bold
+        //     //     )
+        //     //   ),
+        //     child: Icon(
+        //       Icons.favorite,
+        //       color: Colors.white,
+        //       size: 64,
+        //     ),
+        //   ),
+        // ),
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Container(
+              width: 400,
+              color: Colors.deepPurple[400],
+              child: Center(
+                child: Text("koffi"),
+              ),
+            ), 
+            Container(
+              width: 400,
+              color: Colors.deepPurple[300],
+              child: Center(
+                child: Text("kan"),
+              ),
+            ), 
+            Container(
+              width: 400,
+              color: Colors.deepPurple[200],
+              child: Center(
+                child: Text("marc"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class for home 
+// class HomeView extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext){
+//     return 
+//   }
+// }
