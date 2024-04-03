d3f642d44dcac7f4ef824e68dbd9c292283d2b18
ikhela-04KK <kankoffi36@gmail.com>
2024-04-03 17:02:52 +0200

    implement todo app + use Expanded for avaoid mistake about rendrering pixel

---
+++ lib/page/home_page.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+import 'package:todo/util/todo_title.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List TodoTile = [
+    ["Make a tutorial", true], 
+    ["Build your own", false], 
+    ["Talk with Jesus", true],
+    ["Build my social experiance",false]
+  ]; 
+
+  // implementez une méthode pour le checkbox 
+  void CheckBoxChanged(bool? value  , int index){}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text("ITodo"),
+        backgroundColor: Colors.yellow,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: TodoTile.length,
+        itemBuilder: (context, index) {
+          return ToDoTitle(
+            taskName: TodoTile[index][0], 
+            taskCompleted: TodoTile[index][1], 
+            onChanged: (value) =>CheckBoxChanged(value, index));
+        },
+      )
+    );  
+  }
+}
---
+++ lib/util/todo_title.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class ToDoTitle extends StatelessWidget {
+  // create instance for my variable 
+
+  final String taskName;
+  Function(bool?)? onChanged; 
+  final bool taskCompleted; 
+
+  ToDoTitle({super.key, required this.taskName, required this.taskCompleted, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:EdgeInsets.all(25), 
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: <Widget>[
+            Checkbox(
+              value: taskCompleted, 
+              onChanged: onChanged, 
+              ),
+            Text("Make tutorial"), 
+          ],
+        ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.yellow
+      ),
+    )
+    );
+    
+    
+  }
+}
