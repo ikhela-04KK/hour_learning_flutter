7f02829ee117880abd12d576ab0c1389c4dcf81f
ikhela-04KK <kankoffi36@gmail.com>
2024-04-03 23:26:21 +0200

    initialize the class for load + update + add default data to Hive database

---
+++ lib/data/database.dart
@@ -0,0 +1,29 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDatabase{
+  List toDoList = []; 
+
+  // creer une reference à ta boîte 
+  final _todoBox = Hive.box('todoBox'); 
+
+  // stocker des données par defaut dans la boîte 
+  void createInitialData(){
+    toDoList =[
+      ["Make Tutorial", false],
+      ["Do Exercise", false],
+    ];
+  }
+
+  // chargé les données associés à TODOLIST associé à la boîte todoBox
+  void loadData(){
+    toDoList = _todoBox.get("TODOLIST");
+  }
+
+  // modifié la base de donné
+
+  void updateDatabase(){
+    _todoBox.put("TODOLIST", toDoList); 
+  }
+  
+  }
+
